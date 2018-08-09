cf367853ed95f7d7a84c0db6e9cc74164874f572
Ivan Neradovic <i.neradovic@levi9.com>
2018-08-09 15:18:22 +0200

    freecodecamp | 01 01 Basic HTML and HTML5 exrcises

---
+++ README.md
@@ -0,0 +1,8 @@
+# FreeCodeCamp
+FreeCodeCamp exercises repo
+
+--------------------------
+
+| 01 | Responsive Web Design Certification               |    Finished |
+|:---|:--------------------------------------------------|------------:|
+| 01 | Basic HTML and HTML5                              |  09.08.2018 |
---
+++ php-create-files-and-folders/mashine.php
@@ -0,0 +1,22 @@
+<?php
+
+$html = file_get_contents('./list-block.html');
+
+$dom = new domDocument;
+libxml_use_internal_errors(true);
+$dom->loadHTML($html);
+$dom->preserveWhiteSpace = false;
+
+$ext = '.css';
+
+$files = $dom->getElementsByTagName('a');
+
+$i = -1;
+foreach ($files as $file) {
+	$i++;
+	if ($i === 0) continue;
+
+	$f = ($i < 10 ? '0' : '').$i . '-' . str_replace(' ', '-', $file->nodeValue) . $ext;
+
+  	fopen($f, "w");
+}
